5758178dce921106598aa3181fb21fbc4b582054
Tony Gorb <30474304+tonygorb@users.noreply.github.com>
2018-06-17 19:51:13 +0300

    Mathematics init + declare

--- app/src/main/java/wagie/com/wagie/SecondActivity.java
+++ app/src/main/java/wagie/com/wagie/SecondActivity.java
@@ -19,12 +19,11 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
     double hourlyRate;
 
     double dailyHours = 8.36;
-    double dailySum;
-    String dailyValue;
+    double dailySum, weeklySum, monthlySum, yearlySum;
+    String dailyValue, weeklyValue, monthlyValue, yearlyValue;
 
-    TextView daily, weekly, mothly, yearly;
-
-    Button btnDaily;
+    TextView daily, weekly, monthly, yearly;
+    Button btnDaily, btnWeekly, btnMonthly, btnYearly;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +32,8 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
 
         Intent intent = getIntent();
         hValue = intent.getStringExtra("Hourly_Rate");
-
         daily = findViewById(R.id.daily_result);
         daily.setVisibility(View.INVISIBLE);
-
         hourlyRate = Double.parseDouble(hValue);
 
         dailySum = hourlyRate * dailyHours;
@@ -53,26 +50,76 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
             }
         });
 
+
+
+        weekly = findViewById(R.id.weekly_result);
+        weekly.setVisibility(View.INVISIBLE);
+
+        weeklySum = dailySum * 5;
+        weeklyValue = String.valueOf(weeklySum);
+
+        btnWeekly = findViewById(R.id.btn_weekly_result);
+        btnWeekly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                weekly.setVisibility(View.VISIBLE);
+                weekly.setText(weeklyValue);
+            }
+        });
+
+
+        monthly = findViewById(R.id.monthly_result);
+        monthly.setVisibility(View.INVISIBLE);
+
+        monthlySum = weeklySum * 4;
+        monthlyValue = String.valueOf(monthlySum);
+
+        btnMonthly = findViewById(R.id.btn_monthly_result);
+        btnMonthly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                monthly.setVisibility(View.VISIBLE);
+                monthly.setText(monthlyValue);
+            }
+        });
+
+
+        yearly = findViewById(R.id.yearly_result);
+        yearly.setVisibility(View.INVISIBLE);
+
+        yearlySum = monthlySum * 12;
+        yearlyValue = String.valueOf(yearlySum);
+
+        btnYearly = findViewById(R.id.btn_yearly_result);
+        btnYearly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                yearly.setVisibility(View.VISIBLE);
+                yearly.setText(yearlyValue);
+            }
+        });
+
+
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btn_daily_result:
-
-                break;
-
-            case R.id.weekly_result:
-
-                break;
-
-            case R.id.monthly_result:
-
-                break;
-
-            case R.id.yearly_result:
-
-                break;
-        }
+//        switch (v.getId()){
+//            case R.id.btn_daily_result:
+//
+//                break;
+//
+//            case R.id.weekly_result:
+//
+//                break;
+//
+//            case R.id.monthly_result:
+//
+//                break;
+//
+//            case R.id.yearly_result:
+//
+//                break;
+//        }
     }
 }
